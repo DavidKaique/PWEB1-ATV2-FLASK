2f2629ecd9f8c0c05c6e006ebb52059a64b1cb26
David Kaíque <115568812+DavidKaique@users.noreply.github.com>
2023-05-03 21:31:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,25 +4,28 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    return '<a href="/sobre">sobre</a>' \
-           '<br>' \
-           '<a href="/experiencia">experiencia</a>' \
-           '<br>' \
-           '<a href="/formacao">formacao</a>' \
-           '<br>' \
-           '<a href="/contato">contato</a>' \
+    return 'Conheça o meu curriculo clicando abaixo:' \
+        '<br>' \
+        '<br>' \
+        '<a href="/sobre">Sobre</a>' \
+        '<br>' \
+        '<a href="/experiencia">Experiencia</a>' \
+        '<br>' \
+        '<a href="/formacao">Formacão</a>' \
+        '<br>' \
+        '<a href="/contato">Contato</a>' \
 
 @app.route("/sobre", methods=["GET"])
 def sobre():
-    return 'Meu nome é David Kaique e atualmente estou cursando Sistemas de Informação no IFCE Campus Cedro.'
+    return 'Meu nome é David Kaique, tenho 21 anos e estou pronto para obter novos conhecimentos no mercado de TI.'
 
 @app.route("/experiencia", methods=["GET"])
 def experiencia():
-    return 'Tenho o curso técnico de administração feito na EEEP José Iran Costa e estágio supervisonado no Cartório Eleitoral de Várzea Alegre.'
+    return 'Tenho o curso técnico de administração feito na EEEP Dr. José Iran Costa e estágio supervisonado no Cartório Eleitoral de Várzea Alegre.'
 
 @app.route("/formacao", methods=["GET"])
 def formacao():
-    return 'Estou cursando o ensino superior.'
+    return 'Atualmente estou cursando Sistemas de Informação no IFCE Campus Cedro.'
 
 @app.route("/contato", methods=["GET"])
 def contato():
